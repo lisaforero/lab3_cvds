9509a41f9ccd7bceca022b5f257d17b6e4f1f548
carlos.avellaneda-f <carlos.avellaneda-f@mail.escuelaing.edu.co>
2025-02-05 09:56:15 -0500

    Ultimo Avance de clase

--- src/test/java/edu/eci/cvds/tdd/library/book/BookTest.java
+++ src/test/java/edu/eci/cvds/tdd/library/book/BookTest.java
@@ -3,4 +3,11 @@ package edu.eci.cvds.tdd.library.book;
 import org.junit.jupiter.api.Test;
 
 public class BookTest {
+
+    @Test
+    public void testCreateBook(){
+        String title = "Libro rojo";
+
+    }
+
 }
