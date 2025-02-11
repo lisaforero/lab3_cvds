907ebe1831c8ea14567ceabd7cb3eafb040a67f6
Carlos-Avellaneda <carlos.avellaneda-f@mail.escuelaing.edu.co>
2025-02-11 12:13:41 -0500

    Se trata de llenar toda la covertura

--- src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
+++ src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
@@ -8,11 +8,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 public class LibraryTest {
+
     private Library library;
     private Book book1, book2, book3;
     private User user1, user2;
@@ -95,15 +96,67 @@ public class LibraryTest {
 
     @Test
     public void testReturnLoanWithInvalidLoan() {
+        library.addBook(book1);
         Loan loan = new Loan();
         loan.setBook(book1);
         loan.setUser(user1);
         loan.setStatus(LoanStatus.ACTIVE);
         loan.setLoanDate(LocalDateTime.now());
-        library.addBook(book1);
         library.loanABook(user1.getId(),book1.getIsbn());
         Loan returnedLoan = library.returnLoan(loan);
 
         assertNull(returnedLoan);
+    }
+
+    @Test
+    public void testIsBookAvailable_BookDoesNotExist() {
+        assertFalse(library.isBookAvailable("NonExistingISBN"));
+    }
+
+    @Test
+    public void testIsBookAvailable_BookIsAvailable() {
+        library.addBook(book1);
+        assertTrue(library.isBookAvailable(book1.getIsbn()));
+    }
+
+    @Test
+    public void testIsBookAvailable_BookIsNotAvailable() {
+        library.addBook(book1);
+        library.loanABook(user1.getId(), book1.getIsbn());
+        assertFalse(library.isBookAvailable(book1.getIsbn()));
+    }
+
+    @Test
+    public void testLoanABook_UserAlreadyHasActiveLoanForSameBook() {
+        library.addBook(book1);
+        library.loanABook(user1.getId(), book1.getIsbn());
+        Loan loan2 = library.loanABook(user1.getId(), book1.getIsbn());
+        assertNull(loan2);
+    }
+
+    @Test
+    public void testReturnLoan_LoanDoesNotExist() {
+        Loan nonExistingLoan = new Loan();
+        nonExistingLoan.setBook(book1);
+        nonExistingLoan.setUser(user1);
+        Loan returnedLoan = library.returnLoan(nonExistingLoan);
+        assertNull(returnedLoan);
+    }
+
+    @Test
+    public void testReturnLoan_LoanAlreadyReturned() {
+        library.addBook(book1);
+        Loan loan = library.loanABook(user1.getId(), book1.getIsbn());
+        library.returnLoan(loan);
+        Loan returnedLoanAgain = library.returnLoan(loan);
+        assertNull(returnedLoanAgain);
+    }
+
+    @Test
+    public void testAddUser() {
+        User newUser = new User();
+        newUser.setId("789");
+        newUser.setName("Usuario3");
+        assertTrue(library.addUser(newUser));
     }
 }
